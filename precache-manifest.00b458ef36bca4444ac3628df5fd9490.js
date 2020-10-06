72fa5315bc751549cf74b9d2d55ed463cd8be47f
Jakub <jbbabelek@gmail.com>
2020-10-06 16:52:37 +0200

    Updates

---
+++ precache-manifest.00b458ef36bca4444ac3628df5fd9490.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55ccc8f33f1f0e8dc85f1f6603790a5",
+    "url": "/demos/react-movie-search-demo/index.html"
+  },
+  {
+    "revision": "f259325c70ab71ca9137",
+    "url": "/demos/react-movie-search-demo/static/js/2.64fa471e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demos/react-movie-search-demo/static/js/2.64fa471e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097ddec155703a73e21a",
+    "url": "/demos/react-movie-search-demo/static/js/main.cbc10d1e.chunk.js"
+  },
+  {
+    "revision": "e74fad23ec4b3424f1b9",
+    "url": "/demos/react-movie-search-demo/static/js/runtime-main.5b7cb021.js"
+  }
+]);
